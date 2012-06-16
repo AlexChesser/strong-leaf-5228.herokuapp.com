b89d840670a9113d27d25d056a8492ba240176d4
Alex Chesser <achesser@klick.com>
2012-06-16 14:09:44 -0400

    test index

--- index.php
+++ index.php
@@ -205,7 +205,155 @@ $app_name = idx($app_info, 'name', '');
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
     </script>
-    test    
 
+    <header class="clearfix">
+      <?php if (isset($basic)) { ?>
+      <p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
+
+      <div>
+        <h1>Welcome, <strong><?php echo he(idx($basic, 'name')); ?></strong></h1>
+        <p class="tagline">
+          This is your app
+          <a href="<?php echo he(idx($app_info, 'link'));?>" target="_top"><?php echo he($app_name); ?></a>
+        </p>
+
+        <div id="share-app">
+          <p>Share your app:</p>
+          <ul>
+            <li>
+              <a href="#" class="facebook-button" id="postToWall" data-url="<?php echo AppInfo::getUrl(); ?>">
+                <span class="plus">Post to Wall</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>">
+                <span class="speech-bubble">Send Message</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="facebook-button apprequests" id="sendRequest" data-message="Test this awesome app">
+                <span class="apprequests">Send Requests</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <?php } else { ?>
+      <div>
+        <h1>Welcome</h1>
+        <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
+      </div>
+      <?php } ?>
+    </header>
+
+    <section id="get-started">
+      <p>Welcome to your Facebook app, running on <span>heroku</span>!</p>
+      <a href="https://devcenter.heroku.com/articles/facebook" target="_top" class="button">Learn How to Edit This App</a>
+    </section>
+
+    <?php
+      if ($user_id) {
+    ?>
+
+    <section id="samples" class="clearfix">
+      <h1>Examples of the Facebook Graph API</h1>
+
+      <div class="list">
+        <h3>A few of your friends</h3>
+        <ul class="friends">
+          <?php
+            foreach ($friends as $friend) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($friend, 'id');
+              $name = idx($friend, 'name');
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+              <?php echo he($name); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list inline">
+        <h3>Recent photos</h3>
+        <ul class="photos">
+          <?php
+            $i = 0;
+            foreach ($photos as $photo) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($photo, 'id');
+              $picture = idx($photo, 'picture');
+              $link = idx($photo, 'link');
+
+              $class = ($i++ % 4 === 0) ? 'first-column' : '';
+          ?>
+          <li style="background-image: url(<?php echo he($picture); ?>);" class="<?php echo $class; ?>">
+            <a href="<?php echo he($link); ?>" target="_top"></a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Things you like</h3>
+        <ul class="things">
+          <?php
+            foreach ($likes as $like) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($like, 'id');
+              $item = idx($like, 'name');
+
+              // This display's the object that the user liked as a link to
+              // that object's page.
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($item); ?>">
+              <?php echo he($item); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Friends using this app</h3>
+        <ul class="friends">
+          <?php
+            foreach ($app_using_friends as $auf) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($auf, 'uid');
+              $name = idx($auf, 'name');
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+              <?php echo he($name); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+    </section>
+
+    <?php
+      }
+    ?>
+
+
+    <section id="guides" class="clearfix">
+        bottom
+    </section>
   </body>
 </html>
